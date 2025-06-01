ca4b628a3f16602079c0fc4a1f9ea0611ab5e9f3
Bartosz Ryłko <bartek.rylko@gmail.com>
2025-06-01 17:56:18 +0200

    security (main) missing index.php files added

---
+++ views/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 2025 RetJet
+ *
+ * RETJET - SAAS PLATFORM FOR ONLINE STORE INTEGRATION
+ * PROVIDING SOLUTIONS FOR ONLINE STORE MANAGEMENT, RETURNS, AND CLAIMS.
+ * WE HELP E-COMMERCE BUSINESSES TO IMPROVE THEIR OPERATIONS WITH AUTOMATED
+ * PROCESSES AND SEAMLESS INTEGRATIONS WITH POPULAR E-COMMERCE PLATFORMS.
+ *
+ * YOU ARE NOT ALLOWED TO COPY, MODIFY, OR RESELL THIS MODULE.
+ * IN CASE OF ANY QUESTIONS, RETURNS, OR CLAIMS, PLEASE CONTACT US.
+ *
+ * EN: VISIT OUR PLATFORM FOR MORE INFORMATION AND SUPPORT.
+ * HTTPS://WWW.RETJET.COM
+ *
+ * @author    RetJet
+ * @copyright 2025 RetJet
+ * @license   One-site license (single-site, non-transferable)
+ */
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
