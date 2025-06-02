f58ff0fc3adbee18d8f4a9bb10d3ab4b5e830434
Bartosz Ryłko <bartek.rylko@gmail.com>
2025-06-02 09:16:48 +0200

    style (main) empty line removed after phpdoc

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
  * @copyright 2025 RetJet
  * @license   One-site license (single-site, non-transferable)
  */
-
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
